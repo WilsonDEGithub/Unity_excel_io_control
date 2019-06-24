b227abeab05d97920993897adf4cdf2d9b14ddc1
WilsonDEGithub <297122675@qq.com>
2019-06-24 18:55:05 +0800

    change

--- Assets/Scripts/DOExcel.cs
+++ Assets/Scripts/DOExcel.cs
@@ -5,48 +5,34 @@ using System.Data;
 using System.IO;
 using Excel;
 
-public class DoExcel
-{
+public class DoExcel {
     public static DataSet ReadExcel(string path)
     {
-        FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
-        Debug.LogError("stream    " + stream);
-        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);          //读取2007以后版本
-                                                                                                //IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);     //读取2003以后版本
-
+        Debug.Log(path);
+        FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
         DataSet result = excelReader.AsDataSet();
-        Debug.LogError("result   " + result);
         excelReader.Close();
-
-
-        int columns = result.Tables[0].Columns.Count;
-        int rows = result.Tables[0].Rows.Count;
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                string nvalue = result.Tables[0].Rows[i][j].ToString();
-                Debug.Log(nvalue);
-            }
-        }
+        Debug.Log(excelReader.ResultsCount);
         return result;
+
     }
 
     public static List<DepenceTableData> Load(string path)
     {
-        Debug.LogError(path);
         List<DepenceTableData> _data = new List<DepenceTableData>();
         DataSet resultds = ReadExcel(path);
         int column = resultds.Tables[0].Columns.Count;
         int row = resultds.Tables[0].Rows.Count;
         Debug.LogWarning(column + "  " + row);
-        for (int i = 1; i < row; i++)
+        for(int i=1;i<row;i++)
         {
             DepenceTableData temp_data;
             temp_data.instruct = resultds.Tables[0].Rows[i][0].ToString();
             temp_data.word = resultds.Tables[0].Rows[i][1].ToString();
-            Debug.Log(temp_data.instruct + "  " + temp_data.word);
+            temp_data.winstruct = resultds.Tables[0].Rows[i][2].ToString();
+            temp_data.wword = resultds.Tables[0].Rows[i][3].ToString();
+            Debug.Log(temp_data.instruct + "  " + temp_data.word + "  " + temp_data.winstruct + "  " + temp_data.wword);
             _data.Add(temp_data);
         }
         return _data;
@@ -58,4 +44,7 @@ public struct DepenceTableData
 {
     public string word;
     public string instruct;
+    public string wword;
+    public string winstruct;
 }
+
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-#Unity中Excel文件的读取和写入
-
-#Unity内置资源文件ScriptObject的制作和使用。
+#ExcelReadWrite
